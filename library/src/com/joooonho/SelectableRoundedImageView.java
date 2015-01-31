1824e421fa28fad0bb3496e1b03971a359798a42
Joonho Kim <pungrue26@gmail.com>
2015-01-31 15:49:28 +0900

    Optimize draw() call by making canvas adjustment done only once

--- library/src/com/joooonho/SelectableRoundedImageView.java
+++ library/src/com/joooonho/SelectableRoundedImageView.java
@@ -508,6 +508,7 @@ public class SelectableRoundedImageView extends ImageView {
                 configureBounds(canvas);
                 if (mBorderWidth > 0) {
                     adjustBorderWidthAndBorderBounds(canvas);
+                    adjustCanvasForBorder(canvas);
                     setBorderRadii();
                 }
                 mBoundsConfigured = true;
@@ -515,7 +516,6 @@ public class SelectableRoundedImageView extends ImageView {
 
             if (mOval) {
                 if (mBorderWidth > 0) {
-                    adjustCanvasForBorder(canvas);
                     mPath.addOval(mBounds, Path.Direction.CW);
                     canvas.drawPath(mPath, mBitmapPaint);
                     mPath.reset();
@@ -527,7 +527,6 @@ public class SelectableRoundedImageView extends ImageView {
                 }
             } else {
                 if (mBorderWidth > 0) {
-                    adjustCanvasForBorder(canvas);
                     mPath.addRoundRect(mBounds, mRadii, Path.Direction.CW);
                     canvas.drawPath(mPath, mBitmapPaint);
                     mPath.reset();
